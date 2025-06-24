7ad05988dfc61d779b44d3c73461dff8db8cdacf
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-06-24 14:14:21 +0530

    commit

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -3,7 +3,8 @@ package org.example.services;
 import org.example.entities.User;
 
 public class UserBookingService {
+    private User user;
     public UserBookingService(User user){
-        
+        this.user=user;
     }
 }
