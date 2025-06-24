bd7ff8a2cc95937d96dc8a0675574ee67fe346a0
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-06-24 12:53:29 +0530

    commit

---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,12 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPass;
+    private List<Ticket> ticketsbooked;
+    private String userId;
+    
+}
