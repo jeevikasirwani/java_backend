40751325b14c9e2a0021ce1b633e74a4876083b3
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-06-24 19:03:48 +0530

    commit

--- app/src/main/java/org/example/entities/User.java
+++ app/src/main/java/org/example/entities/User.java
@@ -1,12 +1,75 @@
 package org.example.entities;
 
+import java.util.Collections;
 import java.util.List;
 
 public class User {
+
     private String name;
     private String password;
     private String hashPass;
     private List<Ticket> ticketsbooked;
     private String userId;
-    
+
+// constructor
+    public User() {
+    }
+
+    public User(String username, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId) {
+        this.name = name;
+        this.userId = userId;
+        this.password = password;
+        this.hashPass = hashPass;
+        this.ticketsbooked = ticketsBooked != null ? ticketsBooked : Collections.emptyList();
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHashpass() {
+        return hashPass;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsbooked;
+    }
+
+    public void printTickets(){
+        if(ticketsbooked.isEmpty()){
+            System.out.println("No tickets booked");
+            return;
+        }else{
+            for(Ticket ticket:ticketsbooked){
+                System.out.println(ticket.getTicketInfo());
+            }
+        }
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    // setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsbooked) {
+        this.ticketsbooked = ticketsbooked;
+    }
+
+    public void setHashpass(String hashPass) {
+        this.hashPass = hashPass;
+    }
+
 }
--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,10 +1,43 @@
 package org.example.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 import org.example.entities.User;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class UserBookingService {
+
     private User user;
-    public UserBookingService(User user){
-        this.user=user;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<User> userList;
+    private static final String USERS_PATH = "app/src/main/java/org/example/localDb/users.json";
+
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
+    }
+
+    public Boolean loginUser(){
+        Optional<User> founduser=userlist.stream().filter(user->{
+            return user.getName().equals(user.getName()) &&  UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword())
+        }).findFirst();
+        return founduser.isPresent();
+    } 
+
+     public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
     }
 }
